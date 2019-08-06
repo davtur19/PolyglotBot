c222ee827d07c9a98e46aa51e7c34731235019b4
davtur19 <dav.tur19@gmail.com>
2019-08-07 01:01:39 +0200

    Create bot.c

---
+++ bot.c
@@ -0,0 +1,32 @@
+# /* <?php file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=PHP"); exit;?>
+
+#ifdef random
+""" #" 2>/dev/null
+<<COMMENT1
+""" #"
+#else
+#endif
+
+if 1/2:
+ import urllib.request
+ urllib.request.urlopen("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=PY3").read()
+else:
+ import urllib2
+ urllib2.urlopen("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=PY2").read()
+
+ # */ 
+ 
+#include <stdio.h>
+#include <curl/curl.h>
+
+#ifdef random
+""" #"
+#else
+int main(void){CURL *curl; CURLcode res;curl = curl_easy_init();if(curl) {curl_easy_setopt(curl, CURLOPT_URL, "https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=C");curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);res = curl_easy_perform(curl);curl_easy_cleanup(curl);}return 0;}
+#endif 
+#ifdef random
+COMMENT1
+curl "https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=BASH"
+""" #" 2>/dev/null
+#else
+#endif
