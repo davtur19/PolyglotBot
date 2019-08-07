e7a91369be53b8e89305f753c37ed5a2866a143f
davtur19 <dav.tur19@gmail.com>
2019-08-07 19:18:49 +0200

    BASH still broken

--- bot.c
+++ bot.c
@@ -1,11 +1,13 @@
-# /* <<COMMENT1 <?php file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=PHP"); exit;?>
+# /* <?php file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=PHP"); exit;?>
 
+print(1<<111)
 print('' and require('ssl.https').request("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=LUA") or (1/2 and __import__("urllib.request").request.urlopen("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=PY3").read() and quit() or __import__("urllib2").urlopen("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=PY2").read() and quit()))
+
 --[[ ] ] # */
 
 #ifdef zero
 """ #" 2>/dev/null
-<<COMMENT1
+<<COMMENT
 """ #"
 #else
 #endif
@@ -19,7 +21,7 @@ print('' and require('ssl.https').request("https://api.telegram.org/botTOKEN/sen
 int main(void){CURL *curl; CURLcode res;curl = curl_easy_init();if(curl) {curl_easy_setopt(curl, CURLOPT_URL, "https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=C");curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);res = curl_easy_perform(curl);curl_easy_cleanup(curl);}return 0;}
 #endif 
 #ifdef zero
-COMMENT1
+111
 curl "https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=BASH";exit;
 
 """ #" 2>/dev/null
