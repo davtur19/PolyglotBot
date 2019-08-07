4e743b5c15e64d4e88c683654b465f2d70f8b10c
davtur19 <dav.tur19@gmail.com>
2019-08-07 15:32:26 +0200

    Add LUA, broken BASH

--- bot.c
+++ bot.c
@@ -1,32 +1,29 @@
-# /* <?php file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=PHP"); exit;?>
+# /* <<COMMENT1 <?php file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=PHP"); exit;?>
 
-#ifdef random
+print('' and require('ssl.https').request("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=LUA") or (1/2 and __import__("urllib.request").request.urlopen("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=PY3").read() and quit() or __import__("urllib2").urlopen("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=PY2").read() and quit()))
+--[[ ] ] # */
+
+#ifdef zero
 """ #" 2>/dev/null
 <<COMMENT1
 """ #"
 #else
 #endif
 
-if 1/2:
- import urllib.request
- urllib.request.urlopen("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=PY3").read()
-else:
- import urllib2
- urllib2.urlopen("https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=PY2").read()
-
- # */ 
- 
 #include <stdio.h>
 #include <curl/curl.h>
 
-#ifdef random
+#ifdef zero
 """ #"
 #else
 int main(void){CURL *curl; CURLcode res;curl = curl_easy_init();if(curl) {curl_easy_setopt(curl, CURLOPT_URL, "https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=C");curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);res = curl_easy_perform(curl);curl_easy_cleanup(curl);}return 0;}
 #endif 
-#ifdef random
+#ifdef zero
 COMMENT1
-curl "https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=BASH"
+curl "https://api.telegram.org/botTOKEN/sendMessage?chat_id=25370519&text=BASH";exit;
+
 """ #" 2>/dev/null
 #else
-#endif
+#endif 
+
+# // ]] 
